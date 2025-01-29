44b2b56320e70dc1b8c6d5739deb2ddb883f9c34
devenkaler <156245406+devenkaler@users.noreply.github.com>
2025-01-29 01:37:22 -0500

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -12,6 +12,8 @@ def addSchedule(term, class_name):
     with open(schedule_location, 'w') as wf:
         json.dump(data, wf)
 
+    print('Course added succesfully')
+
 
 #removes class from schedule
 def remSchedule(class_name):
@@ -19,8 +21,9 @@ def remSchedule(class_name):
         data = json.load(rf)
     
     for t in data.keys():
-        if (len(data[t]) == 0): break
-        idx = data[t].index(str(class_name))
+        try: idx = data[t].index(str(class_name))
+        except: break
+        
         if idx != -1:
             data[t].pop(idx)
 
@@ -30,7 +33,7 @@ def remSchedule(class_name):
 
 #resets schedule
 def resetSchedule():
-    data  = {"A1": [], "A2": [], "B1": [], "B2": [], "C1": [], "C2": [], "D1": [], "D2": [], "E1": [], "E2": []}
+    data  = {"1A": [], "1B": [], "2A": [], "2B": [], "3A": [], "3B": [], "4A": [], "4B": [], "5A": [], "5B": []}
 
     with open(schedule_location, 'w') as wf:
         json.dump(data, wf)
@@ -46,24 +49,29 @@ def printSchedule():
     with open(schedule_location) as rf:
         data = json.load(rf)
 
-    for i in range(10*len(data.keys())): print('-',end='')
+    for i in range(14*len(data.keys())): print('-',end='')
     print()
 
-    for k in data.keys(): print('|   '+k+'   |',end='')
+    for k in data.keys(): print('|     '+k+'     |',end='')
+    print()
+    for i in range(14*len(data.keys())): print('-',end='')
     print()
 
     while end == False:
         end = True
         for k in data.keys():
             if idx >= len(data[k]): 
+                print('|            |',end='')
                 continue
 
             print('|'+data[k][idx],end='')
-            for i in range(8-len(data[k][idx])): print(' ',end='')
+            for i in range(12-len(data[k][idx])): print(' ',end='')
             print('|',end='')
 
             end = False
 
         idx += 1
+        print()
 
+    for i in range(14*len(data.keys())): print('-',end='')
     print('\n\n')
